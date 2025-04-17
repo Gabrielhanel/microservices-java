f4cb9332866c58eff1d8c0b291e3a1f2d2beb813
Gabriel <ghanel527@gmail.com>
2025-04-17 00:57:36 -0300

    Criação do projeto

--- greating-service/src/main/java/br/edu/atitus/greating_service/GreatingController.java
+++ greating-service/src/main/java/br/edu/atitus/greating_service/GreatingController.java
@@ -8,22 +8,31 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.edu.atitus.greating_service.configs.GreatingConfig;
+
 @RestController
 @RequestMapping("/greating-service")
 public class GreatingController {
 	
-	@Value("${greating-service.greating}")
-	private String greating;
-	
-	@Value("${greating-service.default-name}")
-	private String defaultName;
+//	@Value("${greating-service.greating}")
+//	private String greating;
+//	
+//	@Value("${greating-service.default-name}")
+//	private String defaultName;
 
+	private final GreatingConfig greatingConfig;
+	
 	@GetMapping({"", "/", "{namePath}"})
-	public ResponseEntity<String> getGreating
-	(@PathVariable(required = false) String namePath, @RequestParam(required = false) String name) {
-		if (name == null)
-			name = namePath != null ? namePath : defaultName;
-		String returnText  = String.format(greating, name);
-		return ResponseEntity.ok(returnText);
+	public ResponseEntity<String> getGreating(@PathVariable(required = false) String namePath, @RequestParam(required = false) String name) {
+	    if (name == null)
+	        name = namePath != null ? namePath : greatingConfig.getDefaultName();
+	    String returnText = String.format("%s %s!", greatingConfig.getGreating(), name);
+	    return ResponseEntity.ok(returnText);
+	}
+
+
+	public GreatingController(GreatingConfig greatingConfig) {
+		super();
+		this.greatingConfig = greatingConfig;
 	}
 }
---
+++ greating-service/src/main/java/br/edu/atitus/greating_service/configs/GreatingConfig.java
@@ -0,0 +1,25 @@
+package br.edu.atitus.greating_service.configs;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties("greating-service")
+
+public class GreatingConfig {
+	
+	public String getGreating() {
+		return greating;
+	}
+	public void setGreating(String greating) {
+		this.greating = greating;
+	}
+	public String getDefaultName() {
+		return defaultName;
+	}
+	public void setDefaultName(String defaultName) {
+		this.defaultName = defaultName;
+	}
+	private String greating;
+	private String defaultName;
+}
